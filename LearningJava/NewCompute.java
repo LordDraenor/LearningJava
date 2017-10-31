d3d04fa4c3c6d6387f483333142b62886e97892f
Lucian Stoicescu <lucian-dorel.stoicescu@haufe-lexware.com>
2017-10-31 17:35:18 +0200

    latest

--- LearningJava/NewCompute.java
+++ LearningJava/NewCompute.java
@@ -1,27 +1,25 @@
 import java.util.*;
 
 /**
- * 
  * @author Lucian
  * Learning about variables and declaration
- *
  */
 public class NewCompute {
   public static void main(String[] args){	  	
 	 //radius();
-	 int i = 1;
-	 int j = 2;
-	 System.out.println(i*Math.pow(j, 2));
-     //System.out.println("25/4 = " + 25/4);
-	 double x = 1 -0.1-0.1-0.1-0.1-0.1;
-	 ifs(x); 
-	   
-  }  
-	  
-  
-  
-  
-  
+//	 int i = 1;
+//	 int j = 2;
+//	 System.out.println(i*Math.pow(j, 2));
+//     //System.out.println("25/4 = " + 25/4);
+//	 double x = 1 -0.1-0.1-0.1-0.1-0.1;
+//	 ifs(x); 
+//	 que();
+	 String a = "baaaa";
+	 String b = "qqqqq";
+	 System.out.println(a.compareTo(b));	
+	 System.out.println(1 + "Welcome " + ('\u0001' + 1));
+  } 	
+   
   public static void radius(){
 	      Scanner input; //= new Scanner(System.in);
 	      input = new Scanner(System.in);
@@ -36,6 +34,7 @@ public class NewCompute {
 		  input.close();		  
 		  
 	  }
+  
   public static void ifs(double x){
 	  final double EPSILON = 1E-14;	  
 	  if (Math.abs(x-0.5)<EPSILON)
@@ -43,4 +42,15 @@ public class NewCompute {
 	  
   }
   
+  public static void que(){
+	  char c = 'A';
+	  int i = (int)c;
+	  float f = 1000.34f;
+	   i = (int)f;
+	  double d = 1000.34;
+	   i = (int)d;
+	   i = 97;
+	   c = (char)i;
+	   System.out.println("que");
+  }
 }
