f02505548cdaa1d2eaa72f3aecb43749f12c5faa
StoicescuL <WorkPlace4>
2018-10-01 17:45:06 +0300

    some objects man

--- LearningJava/exercises/ObjectsMan.java
+++ LearningJava/exercises/ObjectsMan.java
@@ -1,5 +1,22 @@
 package exercises;
 
 public class ObjectsMan {
+	int length = 20;
 
+	ObjectsMan(){		
+	}
+	
+	ObjectsMan(int qqq){
+		length = qqq;		
+	}
+	
+	public void printLength() {
+		System.out.println("The length is "+length);		
+	}
+	
+	public static void main(String args[]) {
+		ObjectsMan dude = new ObjectsMan(35);
+		dude.printLength();		
+	}
+	
 }
