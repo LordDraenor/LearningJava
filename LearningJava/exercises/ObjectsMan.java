c774246f9dd78ab9b8f49b5b0a0bee5b27014a32
LordDraenor <lucian7stoicescu@gmail.com>
2018-10-09 02:26:01 +0300

    Nice first try

--- LearningJava/exercises/ObjectsMan.java
+++ LearningJava/exercises/ObjectsMan.java
@@ -1,22 +1,55 @@
 package exercises;
 
-public class ObjectsMan {
-	int length = 20;
+import java.sql.Time;
+import java.util.Calendar;
 
-	ObjectsMan(){		
+public class ObjectsMan {
+	private long startTime;
+	private long  endTime;
+
+	ObjectsMan(){
+		startTime = System.currentTimeMillis();
+	}
+	public void start() {
+		startTime = System.currentTimeMillis();
+	}
+	public void stop() {
+		endTime = System.currentTimeMillis();
 	}
 	
-	ObjectsMan(int qqq){
-		length = qqq;		
+	public long getStartTime() {
+		return this.startTime;
+	} 
+	public long getEndTime() {
+		return this.endTime;
 	}
 	
-	public void printLength() {
-		System.out.println("The length is "+length);		
+	public void getElapsedTime() {
+		System.out.println("The elapsed time is currently" + (this.endTime-this.startTime));		
 	}
+	
 	
 	public static void main(String args[]) {
-		ObjectsMan dude = new ObjectsMan(35);
-		dude.printLength();		
+		long[] random = new long[100000];
+		long temp = 0;
+		for (int i = 0; i < random.length-1; i++) {
+			random[i]=(long)Math.random()*100000;
+		}
+		ObjectsMan currentTime = new ObjectsMan();		
+		for (int i = 0; i < random.length-1; i++) {			
+			long min = random[i];
+			for (int j = 0; j < random.length-1; j++) {
+				if(random[j]<min)
+					temp=min;
+					min=random[j];
+				    random[j]=temp;
+			}
+		}
+		currentTime.stop();
+		currentTime.getElapsedTime();
+		
 	}
+	
+
 	
 }
