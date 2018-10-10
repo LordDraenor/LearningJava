3dddb346abd372edac68001bda81c8f32c8978c4
LordDraenor <lucian7stoicescu@gmail.com>
2018-10-10 22:51:27 +0300

    Works

--- LearningJava/exercises/ObjectsMan.java
+++ LearningJava/exercises/ObjectsMan.java
@@ -2,54 +2,60 @@ package exercises;
 
 import java.sql.Time;
 import java.util.Calendar;
+import java.util.Random;
 
-public class ObjectsMan {
-	private long startTime;
-	private long  endTime;
+class ManObjects {
+    private long startTime;
+    private long  endTime;
 
-	ObjectsMan(){
-		startTime = System.currentTimeMillis();
-	}
-	public void start() {
-		startTime = System.currentTimeMillis();
-	}
-	public void stop() {
-		endTime = System.currentTimeMillis();
-	}
-	
-	public long getStartTime() {
-		return this.startTime;
-	} 
-	public long getEndTime() {
-		return this.endTime;
-	}
-	
-	public void getElapsedTime() {
-		System.out.println("The elapsed time is currently" + (this.endTime-this.startTime));		
-	}
-	
-	
-	public static void main(String args[]) {
-		long[] random = new long[100000];
-		long temp = 0;
-		for (int i = 0; i < random.length-1; i++) {
-			random[i]=(long)Math.random()*100000;
-		}
-		ObjectsMan currentTime = new ObjectsMan();		
-		for (int i = 0; i < random.length-1; i++) {			
-			long min = random[i];
-			for (int j = 0; j < random.length-1; j++) {
-				if(random[j]<min)
-					temp=min;
-					min=random[j];
-				    random[j]=temp;
-			}
-		}
-		currentTime.stop();
-		currentTime.getElapsedTime();
-		
-	}
-	
-
-	
+    ManObjects(){
+        startTime = System.currentTimeMillis();
+    }
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+    
+    public long getStartTime() {
+        return this.startTime;
+    } 
+    public long getEndTime() {
+        return this.endTime;
+    }
+    
+    public void getElapsedTime() {
+        System.out.println("The elapsed time in miliseconds is: " + (this.endTime-this.startTime));        
+    }
+}    
+class ObjectsMan{
+ public static void main(String args[]) {
+        int[] random = new int[10000];
+        int temp = 0;
+        Random seed;
+        for (int i = 0; i < random.length; i++) {
+        	seed = new Random();
+            random[i]=seed.nextInt(100000);
+        }
+        ManObjects currentTime = new ManObjects();        
+        for (int i = 0; i < random.length; i++) {            
+            int min = random[i];
+            for (int j = i+1; j < random.length; j++) {
+                if(random[j]<min) {
+                	random[i]=random[j];
+                    random[j]=min;
+                    min=random[i];
+                }
+                    
+            }
+        }
+        currentTime.stop();
+        for (int i = 0; i < random.length; i++) {
+			System.out.println(random[i]+" ");
+		}        
+        currentTime.getElapsedTime();
+        
+    }
+    
 }
