50920b9c0321019c0d41b080308412d0c6c197c2
Lucian <Lucian@MOBILE-BASE>
2019-09-07 21:02:26 -0400

    Inheritance exercises

--- LearningJava/Inheritance/Excercise3.java
+++ LearningJava/Inheritance/Excercise3.java
@@ -1,11 +1,56 @@
 package Inheritance;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Excercise3 {
-	public static void main(String[] args) {
+	public static void cry(String[] args) {
 		Object circle1 = new CircleFromSimpleGeometricObjectType1(1);
 		Object circle2 = new CircleFromSimpleGeometricObjectType1(1);
 		System.out.println(circle1.equals(circle2));
 	}
+
+	public static void main(String[] args) {
+		ArrayList<String> cityList = new ArrayList<>();
+		cityList.add("London");
+		cityList.add("Beijing");
+		cityList.add("Washington D.C.");
+		System.out.println(cityList.size());
+		System.out.println(cityList.get(2));
+		System.out.println("Is Miami in the list? " + cityList.contains("Miami"));
+		cityList.add("Xian");
+		ArrayList<String> cityList2 = new ArrayList<>();
+		cityList2.add("Miami");
+		cityList2.add("New York");
+		cityList2.add("Moscow");
+		cityList.addAll(cityList2);
+		for (int i = 0; i < cityList.size(); i++) {
+			System.out.println(cityList.get(i));
+		}
+		
+		ArrayList<CircleFromSimpleGeometricObjectType1> circles = new ArrayList<>();
+		circles.add(new CircleFromSimpleGeometricObjectType1(3));
+		circles.add(new CircleFromSimpleGeometricObjectType1(1.5, true, "green"));
+		
+		for (int i = 0; i < circles.size(); i++) {
+			System.out.println("The area of circle number " +(i+1)+" is "+ circles.get(i).getArea());
+		}
+		
+		//List from an Array
+		ArrayList<String> places = new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata"));
+		//Array from a list - notice how none of these come from the Array class. They are more complex methods. But both apply to the Array :)
+		String[] placess = new String[places.size()];
+		places.toArray(placess);
+		
+		places.add("Madrid");
+		for (int i = 0; i < places.size(); i++) {
+			System.out.println(places.get(i));
+		}
+		
+		System.out.println(cityList.toString());
+		java.util.Collections.sort(cityList);
+		System.out.println(cityList.toString());		
+		
+	}
+
 }
-
-
