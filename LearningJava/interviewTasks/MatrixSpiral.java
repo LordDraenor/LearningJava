89d30bd82fd33a14c37e8f59a3f6423695fc8975
Lucian <Lucian@MOBILE-BASE>
2019-11-06 12:28:10 -0500

    Remove annotations for this one

--- LearningJava/interviewTasks/MatrixSpiral.java
+++ LearningJava/interviewTasks/MatrixSpiral.java
@@ -17,27 +17,22 @@ public class MatrixSpiral {
 		 int i, k = 0, l = 0; 
 		 int n = x[0].length;
 		 int m = n;
-	        /*  k - starting row index 
-	        m - ending row index 
-	        l - starting column index 
-	        n - ending column index 
-	        i - iterator 
-	        */
+	      
 	  
 	        while (k < m && l < n) { 
-	            // Print the first row from the remaining rows 
+	           
 	            for (i = l; i < n; ++i) { 
 	                System.out.print(x[k][i] + " "); 
 	            } 
 	            k++; 
 	  
-	            // Print the last column from the remaining columns 
+	            
 	            for (i = k; i < m; ++i) { 
 	                System.out.print(x[i][n - 1] + " "); 
 	            } 
 	            n--; 
 	  
-	            // Print the last row from the remaining rows */ 
+	            
 	            if (k < m) { 
 	                for (i = n - 1; i >= l; --i) { 
 	                    System.out.print(x[m - 1][i] + " "); 
@@ -45,7 +40,7 @@ public class MatrixSpiral {
 	                m--; 
 	            } 
 	  
-	            // Print the first column from the remaining columns */ 
+	         
 	            if (l < n) { 
 	                for (i = m - 1; i >= k; --i) { 
 	                    System.out.print(x[i][l] + " "); 
